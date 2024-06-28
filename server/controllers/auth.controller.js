38c5a39e8f328646ec9b95a344b3ece3506ef724
harderavi <ravindra@stavyah.com>
2024-06-29 01:20:24 +0530

    test api

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,7 +1,4 @@
-import AppUser from '../models/appuser.model.js';
-import { errorHandler } from '../utils/error.js';
-import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
+
 
 
 export const signin = async (req, res, next) => {
