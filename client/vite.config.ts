49aa65baff6fbcf0fe9532727e69b63ed13160a6
harderavi <ravindra@stavyah.com>
2024-06-29 02:13:55 +0530

    Front end url load issue

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -15,4 +15,11 @@ export default defineConfig({
   define: {
     'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
 })
