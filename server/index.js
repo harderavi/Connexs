1bb39bbbdaf2deadab17dfa2e651809dd87c7d45
harderavi <ravindra@stavyah.com>
2024-06-29 00:02:13 +0530

    test

--- server/index.js
+++ server/index.js
@@ -7,11 +7,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGODB_URL).then(() => {
-    console.log("MongoDB connected ")
-}).catch(err => cansole.error(err))
-
-
 
 app.get('/', (req, res) => {
     res.send({ message: "App is working get" })
