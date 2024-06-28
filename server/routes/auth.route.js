a5b65a52f18061fd66bb8cf32949144b70554602
harderavi <ravindra@stavyah.com>
2024-06-29 01:43:53 +0530

    Signin get test

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -3,5 +3,8 @@ import { signin } from "../controllers/auth.controller.js";
 
 const router = express.Router()
 router.post('/signin', signin) 
+router.get('/signin', (req,     res)=>{
+    res.json("signin get working")
+}) 
 
 export default router
