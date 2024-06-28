2fab0069583c1c276805f365b12a46b8e944c286
harderavi <ravindra@stavyah.com>
2024-06-29 00:09:02 +0530

    APi fix

--- server/index.js
+++ server/index.js
@@ -1,15 +1,33 @@
 import express from "express";
+import mongoose from "mongoose";
 import cors from "cors"
 import "dotenv/config";
+import authRouter from "./routes/auth.route.js"
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URL).then(() => {
+    console.log("MongoDB connected ")
+}).catch(err => cansole.error(err))
+
+
 
 
 app.get('/', (req, res) => {
     res.send({ message: "App is working get" })
 });
-
+app.use('/api/auth', authRouter)
+// Endpoint to get all user data
+app.get('/users', async (req, res) => {
+    try {
+        const users = await User.find();
+        res.json(users);
+        console.log(users)
+    } catch (err) {
+        res.status(500).json({ error: 'An error occurred' });
+    }
+});
 app.listen('4000', () => {
     console.log('App is running on port 4000');
 })
